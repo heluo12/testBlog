22dfdadb60f6e3b3f793d0d4728416962055dc91
liyang <liyang@cri.cn>
2023-08-25 11:04:51 +0800

    first commit

---
+++ x/blog/client/cli/query_comment.go
@@ -0,0 +1,82 @@
+package cli
+
+import (
+	"strconv"
+
+	"blog/x/blog/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+func CmdListComment() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-comment",
+		Short: "list all comment",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllCommentRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.CommentAll(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdShowComment() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-comment [id]",
+		Short: "shows a comment",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			id, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			params := &types.QueryGetCommentRequest{
+				Id: id,
+			}
+
+			res, err := queryClient.Comment(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
